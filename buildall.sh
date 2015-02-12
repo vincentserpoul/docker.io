5d6a931b10ef3f9151cc5bb4a8d61885a63b9886
vserpoul <vincent@serpoul.com>
2015-02-12 18:30:14 +0800

    adding nano phpfpm

--- buildall.sh
+++ buildall.sh
@@ -1,5 +1,12 @@
 #bin/sh!
-for image in $(ls -d **/*);do docker build -t $image ./$image/;done;
+
+docker build -t vincentserpoul/nginx_builder vincentserpoul/nginx_builder
+docker run vincentserpoul/nginx_builder cat /rootfs.tar > vincentserpoul/nginx/rootfs.tar
+
+docker build -t vincentserpoul/phpfpm_builder vincentserpoul/phpfpm_builder
+docker run vincentserpoul/phpfpm_builder cat /rootfs.tar > vincentserpoul/phpfpm/rootfs.tar
+
+for image in hhvm nginx percona phpfpm;do docker build -t vincentserpoul/$image ./vincentserpoul/$image/;done;
 
 # cleanup intermediate images
 docker images --no-trunc | grep none | awk '{print $3}' | xargs docker rmi
--- vincentserpoul/phpfpm53/README.md
+++
@@ -1,4 +0,0 @@
-docker_phpfpm_53
-================
-
-Dockerfile to build ubuntu server 12.04 with nphp-fpm and php 5.3!
